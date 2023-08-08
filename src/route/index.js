d6cc6933328ce4d9471ee028aeff34d134956f87
Oleksandr-Can <kokotenko@ukr.net>
2023-08-07 20:11:13 -0600

    update 08/07/2023-2

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,8 @@
-// Підключаємо технологію express для back-end сервера
 const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+
 const router = express.Router()
 
 // ================================================================
-
-// router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
@@ -26,13 +23,15 @@ class Product {
 
   constructor(name, price, description) {
     //Унікальне число з 5 цифр, яке потрібно отримати через функцію Math.random
-    this.id = function generateUniqueNumber() {
+    function generateUniqueNumber() {
       let min = 10000
       let max = 99999
       return (
         Math.floor(Math.random() * (max - min + 1)) + min
       )
     }
+
+    this.id = generateUniqueNumber()
 
     //Дата в форматі ISO рядка, яка створена та додана при створенні об’єкта в методі класу constructor
     this.createDate = new Date().toISOString()
@@ -56,6 +55,13 @@ class Product {
     return this.#list.find((product) => product.id === id)
   }
   //Оновлює властивості аргументу data в об’єкт товару, який був знайдений по ID. Можна оновлювати price, name, description
+  static update(product, data) {
+    // Оновити дані продукту
+    product.name = data.name
+    product.price = data.price
+    product.description = data.description
+  }
+
   static updateById = (id, data) => {
     const product = this.getById(id)
 
@@ -82,8 +88,6 @@ class Product {
   }
 }
 
-let myObject = new Product()
-console.log(myObject.id)
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -106,15 +110,16 @@ router.get('/product-create', function (req, res) {
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.post('/product-create', function (req, res) {
-  const { name, price, description } = req.body
+  const { name, price, description, id } = req.body
 
-  const product = new Product(name, price, description)
+  const product = new Product(name, price, description, id)
 
   Product.add(product)
 
   res.render('alert', {
     style: 'alert',
     info: 'Success',
+    discribe: 'Product was created',
   })
 })
 
@@ -129,14 +134,97 @@ router.get('/product-list', function (req, res) {
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('product-list', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'product-list ',
+    style: 'product-list',
     data: {
-      users: {
+      products: {
         list,
         isEmpty: list.length === 0,
       },
     },
   })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-edit', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const { id } = req.query
+
+  const product = Product.getById(Number(id))
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  if (product) {
+    // Вивести дані продукту в полях форми в container/product-edit
+    res.render('product-edit', {
+      style: 'product-edit',
+      data: {
+        products: {
+          product,
+          isEmpty: false,
+        },
+      },
+    })
+  } else {
+    // Відобразити container/alert з інформацією Товар з таким ID не знайдено
+    res.render('alert', {
+      style: 'alert',
+      info: 'Unsuccessfully',
+      discribe: 'No product with this ID found',
+    })
+  }
+
+  // ↑↑ сюди вводимо JSON дані
+})
+
+//=======================================================
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/product-edit', function (req, res) {
+  // Отримати оновлені дані товару з req.body
+  const { name, price, description, id } = req.body
+
+  // Оновити дані в товарі за допомогою id
+  const result = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+
+  res.render('alert', {
+    style: 'alert',
+    info: result ? 'Success' : 'Unsuccessfully',
+    discribe: result
+      ? 'Product was updated'
+      : 'No product with this ID found',
+  })
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-delete', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // const { name, price, description, id } = req.body
+
+  const { id } = req.query
+
+  const result = Product.deleteById(Number(id))
+
+  res.render('alert', {
+    style: 'alert',
+    info: result ? 'Success' : 'Unsuccessfully',
+    discribe: result
+      ? 'Product was deleted'
+      : 'No product with this ID found',
+  })
+
   // ↑↑ сюди вводимо JSON дані
 })
 // ================================================================
